d02901a018277e3138e299c93849c6b5fc3b7c7b
MW <43390816+mwong4@users.noreply.github.com>
2019-06-29 11:36:08 -0400

    Established the basic movement
    Implemented the code for the horizontal movement, the crouching effect and the jumping effect. <PENDING> Still requires the implementation of the visual aspects along with more modifications to the game style and testing for bugs.

---
+++ 2DFightingGame/Assets/Scripts/playerMovement.cs
@@ -0,0 +1,60 @@
+﻿/*
+Date of creation: June 6, 2019
+Date of modification: June 6, 2019
+Author: Max Wong
+Following Brackeys tutorial
+Purpose: To get player inputs in order to perform movements through the "CharacterController2D".
+*/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMovement : MonoBehaviour
+{
+    public CharacterController2D controller;
+
+    //Various input methods for the fixed update function
+    float horizontalMove = 0f;
+    public float runSpeed = 40f;
+    bool jump = false;
+    bool crouch = false;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Get player Input for horizontal movement
+        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+
+        //Get player Input for jump button  
+        if (Input.GetButtonDown("Jump") || Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            jump = true;
+        }
+
+        //Get player Input for crouch button
+        if (Input.GetButtonDown("Crouch"))
+        {
+            crouch = true;
+        }
+        
+        else if (Input.GetButtonUp("Crouch"))
+        {
+            crouch = false;
+        }
+    }
+
+    //Updates at a fixed rate
+    void FixedUpdate() {
+        //Reference player controller and input values
+            controller.Move(horizontalMove * Time.fixedDeltaTime, crouch, jump);
+            jump = false;
+    }
+}
