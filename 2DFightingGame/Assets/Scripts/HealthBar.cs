2b3c1c714bb6e5b46cf561c1a6b576969fd7b5c4
MW <43390816+mwong4@users.noreply.github.com>
2019-07-01 12:29:55 -0400

    Implemented the basic code for the visuals of the healthbar.
    Implemented the basic code for the visuals of the healthbar. Still requires the implementation of the health and damage system along with the visual aspects of the health bars.

---
+++ 2DFightingGame/Assets/Scripts/HealthBar.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthBar : MonoBehaviour
+{
+
+    private Transform bar;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        bar = transform.Find("Bar");
+        //bar.localScale = new Vector3(0.4f, 1);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SetSize(float sizeNormalized)
+    {
+        bar.localScale = new Vector3(sizeNormalized, 1f);
+    }
+
+}
---
+++ 2DFightingGame/Assets/Scripts/TestHealthBar.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestHealthBar : MonoBehaviour
+{
+
+    [SerializeField] private HealthBar healthBar;
+    public float barSize = 1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        healthBar.SetSize(1f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      healthBar.SetSize(barSize);  
+
+      if(Input.GetButtonDown("Jump") == true)
+      {
+        barSize -= 0.2f;
+      }
+    }
+}
